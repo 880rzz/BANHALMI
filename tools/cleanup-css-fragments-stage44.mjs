df8f62a69d6f444e82554865e3e1628bf697df93
V679643 <107166660+880rzz@users.noreply.github.com>
2026-08-07 16:07:30 +0200

    chore(css): remove one-time stage 44 cleanup script

--- tools/cleanup-css-fragments-stage44.mjs
+++
@@ -1,9 +0,0 @@
-import fs from 'node:fs';
-
-const file='assets/css/style.css';
-let css=fs.readFileSync(file,'utf8');
-const matches=css.match(/^\s*-webkit-\s*$/gm)||[];
-if(!matches.length){console.log('No standalone vendor-prefix fragments found.');process.exit(0);}
-css=css.replace(/^\s*-webkit-\s*\n?/gm,'');
-fs.writeFileSync(file,css);
-console.log(`Removed ${matches.length} standalone -webkit- CSS fragments from style.css.`);
